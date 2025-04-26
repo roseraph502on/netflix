666fe9761ca7ef2647abd8d5d05b8bb1f10cb4ae
roseraph502on <sera6768@naver.com>
2025-04-26 23:38:58 +0900

    잘못된 페이지

--- src/page/NotFoundPage/NotFoundPage.jsx
+++ src/page/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Button, Grid, Skeleton } from '@mui/material';
 
 const NotFoundPage = () => {
+
   return (
-    <div>
-      NotFoundPage
-    </div>
+    <Grid container 
+    sx={{ 
+       height: "100vh", 
+      width: "100vw",
+      display: "flex",
+      flexDirection: "column",     
+      justifyContent: "center",    
+      alignItems: "center"
+    }}
+    >
+      <h2>😿 Not found </h2>
+      <Button href='/'>Go HOME</Button>
+    </Grid>
   )
 }
 
